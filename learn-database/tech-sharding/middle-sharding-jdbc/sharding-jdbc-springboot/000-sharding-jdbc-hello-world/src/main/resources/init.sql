7b5739a84e8306ecf389e74b3d7fefa60c5c405b
Snow <snow-official@outlook.com>
2023-04-14 23:38:47 +0800

    ShardingSphere

--- learn-database/tech-sharding/middle-sharding-jdbc/sharding-jdbc-springboot/000-sharding-jdbc-hello-world/src/main/resources/init.sql
+++ learn-database/tech-sharding/middle-sharding-jdbc/sharding-jdbc-springboot/000-sharding-jdbc-hello-world/src/main/resources/init.sql
@@ -71,23 +71,6 @@ CREATE TABLE user_0
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-/*
- * Copyright (C) <2023> <Snow>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 -- table2
 DROP TABLE IF EXISTS user_1;
 CREATE TABLE user_1
