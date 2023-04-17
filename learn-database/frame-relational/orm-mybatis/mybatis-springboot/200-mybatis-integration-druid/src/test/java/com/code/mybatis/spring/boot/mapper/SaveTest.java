a35e36a558a2ebdf019aac8d3703b13d07a6949b
Snow <snow-official@outlook.com>
2023-04-17 17:31:14 +0800

    MyBatis

---
+++ learn-database/frame-relational/orm-mybatis/mybatis-springboot/200-mybatis-integration-druid/src/test/java/com/code/mybatis/spring/boot/mapper/SaveTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) <2023> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.code.mybatis.spring.boot.mapper;
+
+import com.code.mybatis.spring.boot.MybatisApplicationTest;
+import com.code.mybatis.spring.boot.dal.dos.User;
+import com.code.mybatis.spring.boot.dal.mapper.UserMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Snow
+ * @date 2022/7/4 23:30
+ */
+@Slf4j
+class SaveTest extends MybatisApplicationTest {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void simpleTest() {
+        User user = new User().setRecordId(10086L).setName("test").setAge(10);
+
+        userMapper.save(user);
+    }
+
+}
