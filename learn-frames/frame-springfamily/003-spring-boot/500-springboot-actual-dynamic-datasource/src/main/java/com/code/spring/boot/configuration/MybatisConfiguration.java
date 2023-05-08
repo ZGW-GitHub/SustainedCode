a68ecadc2b9770424817efa2d99510d27bf7f32a
Snow <snow-official@outlook.com>
2023-05-08 16:21:32 +0800

    SpringBoot

--- learn-frames/frame-springfamily/003-spring-boot/500-springboot-actual-dynamic-datasource/src/main/java/com/code/spring/boot/configuration/MybatisConfiguration.java
+++ learn-frames/frame-springfamily/003-spring-boot/500-springboot-actual-dynamic-datasource/src/main/java/com/code/spring/boot/configuration/MybatisConfiguration.java
@@ -17,10 +17,13 @@
 
 package com.code.spring.boot.configuration;
 
-import jakarta.annotation.Resource;
+import cn.hutool.core.map.MapUtil;
+import com.code.spring.boot.component.datasource.DynamicDataSource;
+import com.code.spring.boot.component.datasource.DynamicDataSourceEnum;
+import com.zaxxer.hikari.HikariDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -29,21 +32,44 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author Snow
  * @date 2023/2/15 21:03
  */
 @Configuration(proxyBeanMethods = false)
-@EnableConfigurationProperties(MultipleDataSourceProperties.class)
 @MapperScan(basePackages = "com.code.spring.boot.dal.mapper", sqlSessionFactoryRef = "sqlSessionFactory")
 public class MybatisConfiguration {
 
-	@Resource
-	private DataSource dynamicDataSource;
+	@Bean
+	@ConfigurationProperties("jdbc.first")
+	public DataSource firstDataSource() {
+		return new HikariDataSource();
+	}
 
 	@Bean
-	public SqlSessionFactoryBean sqlSessionFactory() throws IOException {
+	@ConfigurationProperties("jdbc.second")
+	public DataSource secondDataSource() {
+		return new HikariDataSource();
+	}
+
+	@Bean
+	public DataSource dynamicDataSource(DataSource firstDataSource, DataSource secondDataSource) {
+		Map<Object, Object> targetDataSources = MapUtil.builder()
+				.put(DynamicDataSourceEnum.DEFAULT, firstDataSource)
+				.put(DynamicDataSourceEnum.FIRST, firstDataSource)
+				.put(DynamicDataSourceEnum.SECOND, secondDataSource)
+				.build();
+
+		DynamicDataSource dataSource = new DynamicDataSource();
+		dataSource.setTargetDataSources(targetDataSources);
+		dataSource.setDefaultTargetDataSource(targetDataSources.get(DynamicDataSourceEnum.DEFAULT));
+		return dataSource;
+	}
+
+	@Bean
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dynamicDataSource) throws IOException {
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(dynamicDataSource);
 
@@ -53,7 +79,7 @@ public class MybatisConfiguration {
 	}
 
 	@Bean
-	public PlatformTransactionManager transactionManager() {
+	public PlatformTransactionManager transactionManager(DataSource dynamicDataSource) {
 		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
 		transactionManager.setDataSource(dynamicDataSource);
 		return transactionManager;
