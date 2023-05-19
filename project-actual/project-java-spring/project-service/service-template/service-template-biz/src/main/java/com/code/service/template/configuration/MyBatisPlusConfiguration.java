640eaedee6d55c94ec5d46a79d05cfdfc5c01604
Snow <snow-official@outlook.com>
2023-05-19 23:08:09 +0800

    Project

---
+++ project-actual/project-java-spring/project-service/service-template/service-template-biz/src/main/java/com/code/service/template/configuration/MyBatisPlusConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) <2023> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.code.service.template.configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Snow
+ * @date 2023/5/19 22:49
+ */
+@Slf4j
+@Configuration
+public class MyBatisPlusConfiguration {
+
+
+	/**
+	 * MyBatis Plus 拦截器
+	 */
+	@Bean
+	public MybatisPlusInterceptor paginationInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		// 添加 MySQL 的分页拦截器
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
+
+}
