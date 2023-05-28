09734ebbc9fa3f96f5167b6bb3cdb6c94edbc98e
Snow <snow-official@outlook.com>
2023-05-28 16:49:30 +0800

    Project

--- project-actual/project-vue/src/router/index.ts
+++ project-actual/project-vue/src/router/index.ts
@@ -3,30 +3,32 @@ import {createRouter, createWebHistory} from "vue-router";
 // 1、引入组件（可选）
 import HomeView from "../views/HomeView.vue";
 
+// 定义的路由：
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: HomeView,
+    },
+    {
+        path: "/about",
+        name: "about",
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import("../views/AboutView.vue"),
+    },
+    {
+        path: "/demo",
+        name: "demo",
+        component: () => import("../views/demo/DemoView.vue"),
+    },
+]
+
 // 2、创建 Router
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    // 定义的路由：
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import("../views/AboutView.vue"),
-        },
-        {
-            path: "/demo",
-            name: "demo",
-            component: () => import("../components/demo/DemoView.vue"),
-        },
-    ],
+    routes: routes,
 });
 
 // 3、暴露 Router
