9a46e3fff0a0ee2cc4966b257f302624f28c00b6
Snow <snow-official@outlook.com>
2023-06-09 23:55:40 +0800

    Project

--- project-actual/project-java-spring/project-framework/framework-starter-xxljob/src/main/java/com/code/framework/xxl/job/XxlJobFrameworkAutoConfiguration.java
+++ project-actual/project-java-spring/project-framework/framework-starter-xxljob/src/main/java/com/code/framework/xxl/job/XxlJobFrameworkAutoConfiguration.java
@@ -17,44 +17,49 @@
 
 package com.code.framework.xxl.job;
 
+import com.code.framework.xxl.job.config.XxlJobConfig;
+import com.code.framework.xxl.job.config.XxlJobExecutorConfig;
 import com.code.framework.xxl.job.job.AbstractJob;
 import com.xxl.job.core.executor.XxlJobExecutor;
 import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import jakarta.annotation.Resource;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * xxl-job 自动配置，当 BeanFactory 中存在 AbstractJob 类型的 Bean 时才配置 xxl-job
- *
  * @author 愆凡
  * @date 2022/6/14 14:34
  */
 @Slf4j
 @Setter
 @Configuration
-@ConditionalOnBean(AbstractJob.class)
+@ComponentScan("com.code.framework.xxl.job")
 public class XxlJobFrameworkAutoConfiguration implements SmartInitializingSingleton, ApplicationContextAware {
-
-	@Value("${xxl-job.address}")
-	private String address;
 
 	private ApplicationContext applicationContext;
 
-	@Bean
-	@ConfigurationProperties("xxl-job.executor")
-	public XxlJobSpringExecutor xxlJobExecutor() {
-		log.info(">>>>>>>>>>> xxl-job config init");
+	@Resource
+	private XxlJobConfig xxlJobConfig;
 
+	@Resource
+	private XxlJobExecutorConfig xxlJobExecutorConfig;
+
+	@Bean
+	public XxlJobSpringExecutor xxlJobExecutor() {
 		XxlJobSpringExecutor executor = new XxlJobSpringExecutor();
-		executor.setAdminAddresses(address);
+		executor.setAdminAddresses(xxlJobConfig.getAddress());
+		executor.setPort(xxlJobExecutorConfig.getPort());
+		executor.setAppname(xxlJobExecutorConfig.getAppname());
+		executor.setLogPath(xxlJobExecutorConfig.getLogPath());
+		executor.setLogRetentionDays(xxlJobExecutorConfig.getLogRetentionDays());
+
+		log.info(">>>>>>>>>>> xxl-job config init : {}", xxlJobExecutorConfig);
 		return executor;
 	}
 
