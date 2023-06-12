f333f690651e2665b84d163145be0fb30544ae7c
Snow <snow-official@outlook.com>
2023-06-12 18:09:17 +0800

    Project

---
+++ project-actual/project-java-spring/project-framework/framework-starter-mybatis/src/main/java/com/code/framework/mybatis/domain/BaseDO.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) <2023> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.code.framework.mybatis.domain;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+/**
+ * @author Snow
+ * @date 2023/6/12 17:38
+ */
+@Data
+@Slf4j
+public class BaseDO {
+
+	private Boolean delete;
+	private Long    creator;
+	private Date    createTime;
+	private Long    updater;
+	private Date    updateTime;
+
+}
