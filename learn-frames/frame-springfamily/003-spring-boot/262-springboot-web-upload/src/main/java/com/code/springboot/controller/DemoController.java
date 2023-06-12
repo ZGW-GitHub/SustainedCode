ad1dd1b62389058bf056e80a048c5464039ad80e
Snow <snow-official@outlook.com>
2023-06-12 17:28:42 +0800

    SpringBoot

---
+++ learn-frames/frame-springfamily/003-spring-boot/262-springboot-web-upload/src/main/java/com/code/springboot/controller/DemoController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) <2023> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.code.springboot.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Snow
+ * @date 2023/6/12 15:34
+ */
+@RestController
+public class DemoController {
+
+	@PostMapping("demo")
+	public String demo(@RequestParam("file") MultipartFile multipartFile) {
+
+
+		return "SUCCESS";
+	}
+
+}
