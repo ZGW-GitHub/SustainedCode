f7c0c248b9690651340247550bd79f8d1407f49e
Snow <snow-official@outlook.com>
2023-06-14 22:48:05 +0800

    Spring Security JWT

---
+++ learn-frames/frame-springfamily/065-other-spring-security/400-spring-s-jwt-hello-world/src/main/java/com/code/spring/security/controller/DemoController.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) <2023> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.code.spring.security.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Snow
+ * @date 2023/4/30 11:47
+ */
+@RestController
+public class DemoController {
+
+	@RequestMapping("sysUser")
+	public String sysUser() {
+		return "hello spring security !";
+	}
+
+	@RequestMapping("visitor")
+	public String visitor() {
+		return "hello visitor !";
+	}
+
+}
