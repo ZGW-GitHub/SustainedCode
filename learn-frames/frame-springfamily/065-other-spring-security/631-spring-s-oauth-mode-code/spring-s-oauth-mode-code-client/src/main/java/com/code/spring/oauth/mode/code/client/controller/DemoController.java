938f81f5287448e9e3ec025f6a07773a84e8485e
Snow <snow-official@outlook.com>
2023-06-17 16:41:49 +0800

    Spring OAuth Mode - Code

--- learn-frames/frame-springfamily/065-other-spring-security/631-spring-s-oauth-mode-code/spring-s-oauth-mode-code-client/src/main/java/com/code/spring/oauth/mode/code/client/controller/DemoController.java
+++ learn-frames/frame-springfamily/065-other-spring-security/631-spring-s-oauth-mode-code/spring-s-oauth-mode-code-client/src/main/java/com/code/spring/oauth/mode/code/client/controller/DemoController.java
@@ -17,12 +17,18 @@
 
 package com.code.spring.oauth.mode.code.client.controller;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +40,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+	@Resource
+	private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
 	/**
 	 * 防止 404
 	 */
@@ -43,10 +52,24 @@ public class DemoController {
 	}
 
 	@GetMapping("currentUser")
-	public String currentUser() {
+	public String oidcIdToken() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		return JSONUtil.toJsonStr(authentication);
+	}
+
+	@GetMapping("currentAuthentication")
+	public String oauth2Token() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken)) {
+			return StrUtil.EMPTY_JSON;
+		}
+
+		OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
+		String authorizedClientRegistrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+		OAuth2AuthorizedClient oAuth2AuthorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(authorizedClientRegistrationId, oAuth2User.getName());
+
+		return JSONUtil.toJsonStr(oAuth2AuthorizedClient);
 	}
 
 	@GetMapping("currentSession")
