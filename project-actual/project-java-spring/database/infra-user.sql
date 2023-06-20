a22eee75cd63a5caa7f26c0584f7c6b5d60fa144
Snow <snow-official@outlook.com>
2023-06-20 18:14:27 +0800

    Project

---
+++ project-actual/project-java-spring/database/infra-user.sql
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) <2023> <Snow>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+BEGIN;
+
+# 创建数据库：infra_user
+create database if not exists `infra_user`;
+use `infra_user`;
+
+# 创建用户表
+drop table if exists `user_info`;
+create table `user_info`
+(
+    `id`         bigint(20) unsigned not null auto_increment comment '主键' primary key,
+    `record_no`  bigint(20) unsigned not null default 0 comment '唯一键',
+    `name`       varchar(10)         not null default '' comment '用户名',
+    `account`    varchar(32)         not null default '' comment '用户账号',
+    `password`   varchar(32)         not null default '' comment '用户密码',
+    `is_del`     tinyint(1) unsigned not null default 0 comment '已删除',
+    `creator`    bigint(20) unsigned not null default 0 comment '创建人',
+    `created_at` datetime            not null default CURRENT_TIMESTAMP comment '创建时间',
+    `updater`    bigint(20) unsigned not null default 0 comment '更新人',
+    `updated_at` datetime            not null default '1000-01-01 00:00:00' on update CURRENT_TIMESTAMP comment '更新时间',
+    constraint uk_record_no unique (record_no),
+    key inx_account (account)
+);
+
+COMMIT;
