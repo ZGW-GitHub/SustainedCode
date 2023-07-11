e9d55a815b1f43333c866ac48f3c951087d3569b
Snow <snow-official@outlook.com>
2023-07-11 21:36:51 +0800

    Spring OAuth Mode - Code

--- learn-frames/frame-springfamily/065-other-spring-security/631-spring-s-oauth-mode-code/spring-s-oauth-mode-code-common/src/main/java/com/code/spring/oauth/mode/code/common/component/RedisSecurityContextRepository.java
+++ learn-frames/frame-springfamily/065-other-spring-security/631-spring-s-oauth-mode-code/spring-s-oauth-mode-code-common/src/main/java/com/code/spring/oauth/mode/code/common/component/RedisSecurityContextRepository.java
@@ -17,14 +17,14 @@
 
 package com.code.spring.oauth.mode.code.common.component;
 
-import cn.hutool.json.JSONUtil;
+import cn.hutool.core.util.StrUtil;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
-import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.SerializationCodec;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
@@ -44,7 +44,7 @@ import java.util.concurrent.TimeUnit;
 public class RedisSecurityContextRepository implements SecurityContextRepository {
 
 	public static final String  SPRING_SECURITY_AUTH_HEADER = "nonce";
-	public static final String  SPRING_SECURITY_CONTEXT_KEY = "security:context:%s";
+	public static final String  SPRING_SECURITY_CONTEXT_KEY = "security:context:{}";
 	public static final Integer DEFAULT_TIMEOUT_SECONDS     = 5 * 60 * 1000;
 
 	private final String defaultNonce = "666";
@@ -76,11 +76,11 @@ public class RedisSecurityContextRepository implements SecurityContextRepository
 		// 如果当前的 context 是空的，则移除
 		SecurityContext emptyContext = this.securityContextHolderStrategy.createEmptyContext();
 		if (emptyContext.equals(context)) {
-			redissonClient.getBucket(SPRING_SECURITY_CONTEXT_KEY + nonce).delete();
+			redissonClient.getBucket(getSpringSecurityContextKey(nonce)).delete();
 		} else {
 			// 保存认证信息
-			RBucket<String> bucket = redissonClient.getBucket(SPRING_SECURITY_CONTEXT_KEY + nonce, StringCodec.INSTANCE);
-			bucket.set(JSONUtil.toJsonStr(context), DEFAULT_TIMEOUT_SECONDS, TimeUnit.MILLISECONDS);
+			RBucket<SecurityContext> bucket = redissonClient.getBucket(getSpringSecurityContextKey(nonce), new SerializationCodec());
+			bucket.set(context, DEFAULT_TIMEOUT_SECONDS, TimeUnit.MILLISECONDS);
 		}
 	}
 
@@ -96,7 +96,7 @@ public class RedisSecurityContextRepository implements SecurityContextRepository
 		}
 
 		// 检验当前请求是否有认证信息
-		return redissonClient.getBucket(SPRING_SECURITY_CONTEXT_KEY + nonce).isExists();
+		return redissonClient.getBucket(getSpringSecurityContextKey(nonce)).isExists();
 	}
 
 	/**
@@ -118,11 +118,16 @@ public class RedisSecurityContextRepository implements SecurityContextRepository
 		}
 
 		// 根据缓存 id 获取认证信息
-		Object contextCache = redissonClient.getBucket(SPRING_SECURITY_CONTEXT_KEY + nonce).get();
-		if (contextCache instanceof String contextStr) {
-			return JSONUtil.toBean(contextStr, SecurityContext.class);
+		RBucket<SecurityContext> bucket = redissonClient.getBucket(getSpringSecurityContextKey(nonce), new SerializationCodec());
+		if (!bucket.isExists()) {
+			return null;
 		}
-		return null;
+
+		return bucket.get();
+	}
+
+	private String getSpringSecurityContextKey(String nonce) {
+		return StrUtil.format(SPRING_SECURITY_CONTEXT_KEY, nonce);
 	}
 
 }
